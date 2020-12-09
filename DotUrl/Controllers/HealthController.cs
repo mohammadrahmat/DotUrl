ef1c4da2ee04634c6923a6162cced806962179f4
mrahmat <mrahmat1991@hotmail.com>
2020-12-09 23:02:15 +0300

    Initial commit

---
+++ DotUrl/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotUrl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Health Controller Get Called. Service is alive.");
+            return "Service is alive.";
+        }
+    }
+}
